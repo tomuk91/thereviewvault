b91ad873389b56795676d5611bd25ef56486436e
AzureAD\TomThornton <thomas.thornton@selectedinterventions.com>
2024-10-19 11:04:11 +0100

    update to sitemaps

--- reviews/sitemaps.py
+++ reviews/sitemaps.py
@@ -1,13 +1,16 @@
 from django.contrib.sitemaps import Sitemap
 from django.urls import reverse
 from .models import Review
+from django.utils import timezone
+
 
 class ReviewSitemap(Sitemap):
     changefreq = "daily"  # The frequency search engines should check for updates
     priority = 0.7  # Priority of the page, 0.0 to 1.0
 
     def items(self):
-        return Review.objects.all()  # Return all published reviews
+        return Review.objects.filter(publication_date__lte=timezone.now())
+
 
     def lastmod(self, obj):
         return obj.modified_date  # Date when the review was last modified
